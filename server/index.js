af2b31ea88f3039a55d6b8347d740bff0d9ef364
Ayush <ayushsoni18182@gmail.com>
2023-01-27 10:40:40 +0530

    clientlink

--- server/index.js
+++ server/index.js
@@ -36,5 +36,5 @@ app.use("/", router);
 io.on("connection", sockets);
 
 httpServer.listen(PORT, () => {
-  console.log("Server is running at http://localhost:4000");
+  console.log("Server is running at https://chatapp-fs35.onrender.com");
 });
